22416ddb8b0ceb44799797b8f9409c63bc15f969
dlufr06 <dlufr06@gmail.com>
2016-09-09 11:22:56 +0200

    updated test.js
    so it works with qunit 2

--- js/test.js
+++ js/test.js
@@ -4,28 +4,30 @@
  * and open the template in the editor.
  */
 
-module("sum");
-test("sum function exist",2,function (){
-    ok(sum,"sum exist");
-    ok(typeof sum === 'function',"sum is a function");
+QUnit.module("sum");
+QUnit.test("sum function exist",function (assert){
+    assert.expect(2);
+    assert.ok(sum,"sum exist");
+    assert.ok(typeof sum === 'function',"sum is a function");
 });
 
-test("sum returns correct",2,function (){
-    equal(sum(1,10),55,"sum returns correct");
-    equal(sum(-5,5),0,"sum returns correct");
+QUnit.test("sum returns correct",function (assert){
+    assert.expect(2);
+    assert.equal(sum(1,10),55,"sum returns correct");
+    assert.equal(sum(-5,5),0,"sum returns correct");
 });
 
-module("is prime");
-test("isprime function exist",2,function (){
-    ok(isprime,"isprime exist");
-    ok(typeof  isprime  === 'function',"isprime exist");
+QUnit.module("isPrime");
+QUnit.test("isprime function exist",function (assert){
+    assert.expect(2);
+    assert.ok(isprime,"isprime exist");
+    assert.ok(typeof  isprime  === 'function',"isprime exist");
 });
 
-test("prime returns correct",2,function (){
-    strictEqual(isprime(599),true,"599 is a prime number");
-    strictEqual(isprime(600),false,"600 is not a prime number");
+QUnit.test("prime returns correct",function (assert){
+    assert.expect(2);
+    assert.strictEqual(isprime(599),true,"599 is a prime number");
+    assert.strictEqual(isprime(600),false,"600 is not a prime number");
 });
 // https://blog.nraboy.com/2015/04/determine-if-a-number-is-prime-using-javascript/
-
-
 
